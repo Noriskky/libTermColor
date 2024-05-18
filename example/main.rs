9414a87b00777a962fbb6069a62040db61357f8f
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-18 22:17:58 +0200

    Fixed Bug in Rust example

--- example/main.rs
+++ example/main.rs
@@ -1,7 +1,7 @@
 use libtermcolor;
 
 fn main() {
-    for (color_name, color_attributes) in &libtermcolor::colors::COLOR_MAP.iter() {
+    for (color_name, color_attributes) in libtermcolor::colors::COLOR_MAP.iter() {
         println!("{}{}{}:", color_attributes.background, color_name, libtermcolor::colors::reset());
         println!("  Regular:    {}{}{}{}", color_attributes.regular, color_name, color_attributes.reset, libtermcolor::colors::reset());
         println!("  Bold:       {}{}{}{}", color_attributes.bold, color_name, color_attributes.reset, libtermcolor::colors::reset());
