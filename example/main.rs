e36a78ce4a51be0128ad85470f72ddb4cf372086
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-08-10 17:27:28 +0200

    Fixed main.rs

--- example/main.rs
+++ example/main.rs
@@ -2,10 +2,10 @@ use libtermcolor::colors;
 
 fn main() {
     for (color_name, color_attributes) in colors::COLOR_MAP.iter() {
-        println!("{}{}{}:", color_attributes.background, color_name, libtermcolor::colors::reset());
-        println!("  Regular:    {}{}{}{}", color_attributes.regular, color_name, color_attributes.reset, libtermcolor::colors::reset());
-        println!("  Bold:       {}{}{}{}", color_attributes.bold, color_name, color_attributes.reset, libtermcolor::colors::reset());
-        println!("  Underline:  {}{}{}{}", color_attributes.underline, color_name, color_attributes.reset, libtermcolor::colors::reset());
-        println!("  Background: {}{}{}{}", color_attributes.background, color_name, color_attributes.reset, libtermcolor::colors::reset());
+        println!("{}{}{}:", color_attributes.background, color_name, colors::reset());
+        println!("  Regular:    {}{}{}{}", color_attributes.regular, color_name, color_attributes.reset, colors::reset());
+        println!("  Bold:       {}{}{}{}", color_attributes.bold, color_name, color_attributes.reset, colors::reset());
+        println!("  Underline:  {}{}{}{}", color_attributes.underline, color_name, color_attributes.reset, colors::reset());
+        println!("  Background: {}{}{}{}", color_attributes.background, color_name, color_attributes.reset, colors::reset());
     }
 }
